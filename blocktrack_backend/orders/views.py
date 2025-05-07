d8d249f961c473255a81d5c0f4a025c72bf6f3c5
Pasindu Ravishan <pasinduravishan88@gmail.com>
2025-05-07 12:36:27 +0530

    database integration + django admin order views are done

--- blocktrack_backend/orders/views.py
+++ blocktrack_backend/orders/views.py
@@ -1,10 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
+from rest_framework import generics
+from .models import Order
+from .serializers import OrderSerializer
 import subprocess
 import tempfile
 import os
 from .ipfs_utils import upload_to_ipfs
+from django_filters.rest_framework import DjangoFilterBackend
 
 FABRIC_BASE = "/Users/ravishan/hyperledger-fabric/fabric-samples"
 BIN_PATH = f"{FABRIC_BASE}/bin"
@@ -121,3 +125,16 @@ peer chaincode query \
                 "error": "Unexpected error",
                 "details": str(e)
             }, status=500)
+
+
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status']
+
+
+class OrderDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    lookup_field = 'order_id'
