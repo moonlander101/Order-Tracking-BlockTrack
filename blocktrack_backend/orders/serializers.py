32262bc3779d9d26ca642cbe6cabeb9066fda467
moonlander101 <himathsamarakoon@gmail.com>
2025-05-08 18:04:27 +0530

    Added new model OrderProduct to add multiple products to order

--- blocktrack_backend/orders/serializers.py
+++ blocktrack_backend/orders/serializers.py
@@ -1,7 +1,37 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, OrderDetails, OrderProduct
+
+
+class OrderProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = ['product_id', 'count', 'unit_price']
+
+class OrderDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderDetails
+        fields = ['warehouse_id', 'nearest_city', 'latitude', 'longitude']
 
 class OrderSerializer(serializers.ModelSerializer):
+    details = OrderDetailsSerializer()
+    products = OrderProductSerializer(many=True)
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['order_id', 'user_id', 'status',
+                  'blockchain_tx_id', 'ipfs_hash', 'created_at',
+                  'details', 'products']
+        read_only_fields = ['order_id', 'created_at']
+
+    def create(self, validated_data):
+        details_data = validated_data.pop('details')
+        products_data = validated_data.pop('products')
+
+        order = Order.objects.create(**validated_data)
+
+        OrderDetails.objects.create(order=order, **details_data)
+
+        for prod in products_data:
+            OrderProduct.objects.create(order_id=order, **prod)
+
+        return order
--- blocktrack_backend/orders/urls.py
+++ blocktrack_backend/orders/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('read-order/<str:order_id>/', ReadOrderView.as_view()),
     path('orders/', OrderListCreateView.as_view(), name='order-list-create'),
     path('orders/<str:order_id>/', OrderDetailView.as_view(), name='order-detail'),
+    # path("api/legacy/order/<str:order_id>/", ReadOrderLegacyView.as_view()),
 ]
