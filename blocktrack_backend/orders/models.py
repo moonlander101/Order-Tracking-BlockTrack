5572d6b91e04ac2b4642666eaa9c1e0d34db639d
moonlander101 <himathsamarakoon@gmail.com>
2025-05-08 18:40:12 +0530

    Updated migrations and Models

--- blocktrack_backend/orders/models.py
+++ blocktrack_backend/orders/models.py
@@ -29,7 +29,7 @@ class OrderDetails(models.Model):
         return f"Order {self.order.order_id} - Details"
 
 class OrderProduct(models.Model):
-    order = models.ForeignKey(Order, related_name='order_products', on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, related_name='order_products', on_delete=models.CASCADE)
     product_id = models.IntegerField()
     count = models.IntegerField()
     unit_price = models.FloatField()
