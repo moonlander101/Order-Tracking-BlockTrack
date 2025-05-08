ed7b1161a927e2af7cf300e2b235357e700e68a9
moonlander101 <himathsamarakoon@gmail.com>
2025-05-08 18:03:23 +0530

    Added route to export swagger.json

--- blocktrack_backend/blocktrack_backend/urls.py
+++ blocktrack_backend/blocktrack_backend/urls.py
@@ -37,4 +37,9 @@ urlpatterns = [
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(
+        'swagger.json',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-swagger-ui-json'
+    ),
 ]
