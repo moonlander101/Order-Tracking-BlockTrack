0a5f98baa6491f19b4491ddb0036a05978547958
moonlander101 <himathsamarakoon@gmail.com>
2025-05-08 14:04:18 +0530

    Updated order.models to have OrderDetails

--- blocktrack_backend/orders/models.py
+++ blocktrack_backend/orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Order(models.Model):
-    order_id = models.CharField(max_length=100, unique=True)
+    order_id = models.AutoField(primary_key=True)
     product = models.CharField(max_length=255)
     customer = models.CharField(max_length=255)
     status = models.CharField(max_length=50, choices=[
@@ -16,3 +16,16 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.order_id} - {self.status}"
+
+class OrderDetails(models.Model):
+    order_id = models.ForeignKey(Order, related_name='details', on_delete=models.CASCADE)
+    product_id = models.IntegerField()
+    count = models.IntegerField()
+    warehouse_id = models.IntegerField()
+    address = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Order {self.order.order_id} - Product {self.product_id}"
