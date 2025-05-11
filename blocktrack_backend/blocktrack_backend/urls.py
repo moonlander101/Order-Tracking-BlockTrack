67722f52be5f0ccdda225721be6bba4b798e8925
moonlander101 <himathsamarakoon@gmail.com>
2025-05-12 01:41:35 +0530

    Added docs urls to main app

--- blocktrack_backend/blocktrack_backend/urls.py
+++ blocktrack_backend/blocktrack_backend/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v0/', include('orders.urls')),
     path('api/v0/', include('supplier_request.urls')),
+    path('api/v0/', include('docs.urls')),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
